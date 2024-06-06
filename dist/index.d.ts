21afbac6201848abb1dfab9da002e76d3331940a
liumingxun <liumingxun@yeah.net>
2024-06-06 10:57:24 +0800

    chore: add dist to ignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { FlavorName } from '@catppuccin/palette';
-export { FlavorName } from '@catppuccin/palette';
-import { CustomTheme } from 'daisyui';
-
-declare function createFlavor(theme: FlavorName): CustomTheme;
-
-export { createFlavor as default };
