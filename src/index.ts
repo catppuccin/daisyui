ffa0deddebadbb6835cfb2de845378f4b49772ff
Liumingxun <liumingxun@yeah.net>
2024-06-24 10:26:06 +0800

    feat: overload api with custom options

--- src/index.ts
+++ src/index.ts
@@ -35,35 +35,53 @@ const themeKeys = [
   'warning-content',
 ] as const
 
-const defaultColorOptions: DefaultColorOptions = {
+const defaultColorOptions: ColorOptions = {
   primary: 'lavender',
   secondary: 'subtext1',
   accent: 'rosewater',
   neutral: 'overlay1',
+  success: 'green',
+  warning: 'yellow',
+  error: 'red',
   info: 'blue',
 }
 
-interface DefaultColorOptions {
+interface ColorOptions {
   primary: AccentName
   secondary: AccentName | MonochromaticName
   accent: AccentName
   neutral: MonochromaticName
+  success: 'green'
+  warning: 'yellow'
+  error: 'red'
   info: AccentName
 }
 
-type CustomColorOptions = Partial<typeof defaultColorOptions>
+type CustomColorOptions = Partial<Omit<ColorOptions, 'success' | 'warning' | 'error'>>
 
-function getSemanticColors(options: CustomColorOptions = {}): typeof defaultColorOptions {
+function getSemanticColors(options: CustomColorOptions = {}): ColorOptions {
   return { ...defaultColorOptions, ...options }
 }
 
-function createFlavor(theme: FlavorName, customColors?: CustomColorOptions): CustomTheme {
+function createFlavor(theme: FlavorName, accent?: AccentName): CustomTheme
+function createFlavor(theme: FlavorName, customColors?: CustomColorOptions): CustomTheme
+function createFlavor(theme: FlavorName, options?: CustomColorOptions | AccentName): CustomTheme {
   const palette = flavorEntries.find(([name]) => name === theme)?.[1]
 
   if (!palette)
     throw new Error(`Flavor ${theme} not found!`)
 
-  const { primary, secondary, accent, neutral, info } = getSemanticColors(customColors)
+  let customColors: ColorOptions
+  if (typeof options === 'string') {
+    customColors = getSemanticColors({
+      accent: options,
+    })
+  }
+  else {
+    customColors = getSemanticColors(options)
+  }
+
+  const { primary, secondary, accent, neutral, info, success, error, warning } = customColors
 
   const daisyTheme: Record<string, Partial<Record<typeof themeKeys[number], string>>> = {
     [theme]: {
@@ -73,9 +91,9 @@ function createFlavor(theme: FlavorName, customColors?: CustomColorOptions): Cus
       'secondary': palette.colors[secondary].hex,
       'accent': palette.colors[accent].hex,
       'neutral': palette.colors[neutral].hex,
-      'success': palette.colors.green.hex,
-      'warning': palette.colors.yellow.hex,
-      'error': palette.colors.red.hex,
+      'success': palette.colors[success].hex,
+      'warning': palette.colors[warning].hex,
+      'error': palette.colors[error].hex,
       'info': palette.colors[info].hex,
     },
   }
