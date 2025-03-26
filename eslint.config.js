57eca85deaaa9fa10ca5fecec416d7480ecbf0f6
Liumingxun <liumingxun@yeah.net>
2025-03-26 09:25:07 +0000

    feat: upgrade eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
-// eslint.config.js
 import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    markdown: false,
-    ignores: ['**/node_modules/**', '**/dist/**'],
-  },
-)
+export default antfu({
+  formatters: true,
+})
