b2f67b0589c25d61ad0fee4eec239b17239fd8ed
Lime <liumingxun@yeah.net>
2025-04-19 13:06:05 +0800

    feat: enhance build configuration and add legacy theme support

--- build.config.ts
+++ build.config.ts
@@ -4,4 +4,12 @@ export default defineBuildConfig({
   externals: [
     'daisyui/theme',
   ],
+  entries: [
+    'src/legacy/index',
+    'src/index',
+  ],
+  rollup: {
+    emitCJS: true,
+  },
+  declaration: true,
 })
