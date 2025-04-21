0298ceecb16f0105b303fcbba1e2747d7f2c94ed
Lime <liumingxun@yeah.net>
2025-04-21 23:31:56 +0800

    feat: enhance CDN support and improve theme generation logic

--- src/generate.ts
+++ src/generate.ts
@@ -4,8 +4,20 @@ import { generateTheme } from './generateTheme'
 
 const flavorNames = ['latte', 'frappe', 'macchiato', 'mocha'] as FlavorName[]
 
-flavorNames.forEach((f) => {
-  const fileName = `./${f}.css`
-  const themeCSS = generateTheme(f)
-  appendFile(fileName, `:root:has(input.theme-controller[value=${f}]:checked),[data-theme="${f}"]${JSON.stringify(themeCSS).replace(/"/g, '').replace(/,/g, ';')}`, { encoding: 'utf-8' })
-})
+const themeCSSs = flavorNames.map(f =>
+  `:root:has(input.theme-controller[value=${f}]:checked),[data-theme="${f}"]${JSON.stringify(generateTheme(f)).replace(/"/g, '').replace(/,/g, ';')}`,
+)
+
+Promise.all(
+  [
+    ...themeCSSs.map((themeCSS, i) => {
+      const flavorName = flavorNames[i]
+      return appendFile(
+        `./${flavorName}.css`,
+        themeCSS,
+        { encoding: 'utf-8' },
+      )
+    }),
+    appendFile('./themes.css', themeCSSs.join('\n'), { encoding: 'utf-8' }),
+  ],
+)
